3079609cbf88358b4cfa3c37b71c20d064f9b06c
Muhammad Mohsin <mohsinking998877@gmail.com>
2025-11-16 06:28:55 +0000

    Data ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,5 +25,22 @@ class DataIngestion:
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info('Train test split initiated')
+            train_set,test_set = train_test_split(df, test_size=0.25, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info('Data Ingestion Completed')
+            
+            return(
+                self.ingestion_config.train_data_path
+                self.ingestion_config.test_data_path
+
+
+
+            )
         except:
 
